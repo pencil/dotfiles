1c7ffb14e6af404a38bbbed16a9dc9bc744d1e27
Nils Caspar <ncaspar@me.com>
2016-09-25 12:59:29 -0700

    rip boxen

--- system/env.zsh
+++ system/env.zsh
@@ -3,12 +3,3 @@ export LC_ALL=en_US.UTF-8
 export DISABLE_SPRING=1 # SPRING – NOT EVEN ONCE
 
 export GREP_OPTIONS='--color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn'
-
-if [ -d "/opt/boxen" ]
-then
-  export BOXEN_HOME='/opt/boxen'
-  export CFLAGS="-I$BOXEN_HOME/homebrew/include"
-  export CPPFLAGS="-I$BOXEN_HOME/homebrew/include"
-  export CPLUS_INCLUDE_PATH="$BOXEN_HOME/homebrew/include"
-  export LIBRARY_PATH="$BOXEN_HOME/homebrew/lib"
-fi
