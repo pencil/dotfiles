2ddfdaac3dc7b89854879e8b4f71ea78296ccd42
Nils Caspar <nils@jumpstart.me>
2021-03-19 13:16:42 -0700

    Fix jq colors

--- system/env.zsh
+++ system/env.zsh
@@ -2,3 +2,6 @@ export LC_ALL=en_US.UTF-8
 export DISABLE_SPRING=1 # SPRING – NOT EVEN ONCE
 
 export GREP_OPTIONS='--color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn'
+
+# https://github.com/stedolan/jq/issues/1252
+export JQ_COLORS="1;90:0;39:0;39:0;39:0;32:1;39:1;39"
