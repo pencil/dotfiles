d28557a08ccf4605b0635bc99bf4c7a8731c757a
Nils Caspar <ncaspar@me.com>
2021-12-08 14:31:13 -0800

    Fix compataiblity with brew on M1

--- go/path.zsh
+++ go/path.zsh
@@ -1,4 +1,7 @@
-export GOROOT="/usr/local/opt/go/libexec"
+if type brew &> /dev/null
+then
+  export GOROOT="$(brew --prefix)/opt/go/libexec"
+fi
 export GOPATH="$HOME/projects/go"
 
 export PATH="$PATH:$GOPATH/bin"
