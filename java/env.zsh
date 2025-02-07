b2bcc4d5d84de42ee7ae2a9841773284f47b23c3
Nils Caspar <nils@outset.ai>
2025-02-07 09:07:21 -0800

    Things

---
+++ java/env.zsh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+# Android SDK
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
