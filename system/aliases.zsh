bf61ed29b19c38192662dc9f433e638baacee93c
Nils Caspar <nils@outset.ai>
2026-05-04 10:27:11 -0700

    ssh tmux gone

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,32 +6,3 @@ alias nscp='scp -o StrictHostKeyChecking=false -o UserKnownHostsFile=/dev/null'
 # development
 alias jsc='/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Helpers/jsc'
 alias history='history 1'
-
-# ssh wrapper that rename current tmux window to the hostname of the
-# remote host.
-ssh() {
-  # Do nothing if we are not inside tmux or ssh is called without arguments
-  if [[ $# == 0 || -z $TMUX ]]; then
-    command ssh $@
-    return
-  fi
-
-  local remote=""
-  while [ $# -gt 0 ]; do
-    case "$1" in
-      -*) shift ;;
-      *)  remote="$(echo ${1} | cut -f1 -d.)"; break ;;
-    esac
-  done
-  local old_name="$(tmux display-message -p '#W')"
-  local renamed=0
-  # Save the current name
-  if [[ $remote != -* ]]; then
-    renamed=1
-    tmux rename-window $remote
-  fi
-  command ssh $@
-  if [[ $renamed == 1 ]]; then
-    tmux rename-window "$old_name"
-  fi
-}
